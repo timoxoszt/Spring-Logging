e00f34479842c8f1974b500fca77e718bb35d913
timoxoszt <20520810@gm.uit.edu.vn>
2022-09-08 00:30:30 +0700

    AppController

--- src/main/java/com/likelion/springlogging/controller/AppController.java
+++ src/main/java/com/likelion/springlogging/controller/AppController.java
@@ -1,2 +1,24 @@
-package com.likelion.springlogging.controller;public class AppController {
+package com.likelion.springlogging.controller;
+
+import com.likelion.springlogging.dto.DepartmentDto;
+import com.likelion.springlogging.dto.EmployeeDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/likelion")
+public class AppController {
+    @PostMapping("/employeeDto")
+    public String validEmployeeDto(@RequestBody @Valid EmployeeDto employeeDto){
+        return "employeeDto is valid";
+    }
+
+    @PostMapping("/departmentDto")
+    public String validDepartmentDto(@RequestBody @Valid DepartmentDto departmentDto){
+        return "departmentDto is valid";
+    }
 }
