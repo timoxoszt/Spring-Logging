146ce55a4637bda7d02b490c24c4e557da14c9e5
timoxoszt <20520810@gm.uit.edu.vn>
2022-09-08 00:57:44 +0700

    implements service

--- src/main/java/com/likelion/springlogging/service/Imp/DepartmentServiceImp.java
+++ src/main/java/com/likelion/springlogging/service/Imp/DepartmentServiceImp.java
@@ -1,2 +1,13 @@
-package com.likelion.springlogging.service.Imp;public class DepartmentServiceImp {
+package com.likelion.springlogging.service.Imp;
+
+import com.likelion.springlogging.dto.DepartmentDto;
+import com.likelion.springlogging.service.DepartmentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentServiceImp implements DepartmentService {
+    @Override
+    public DepartmentDto getDepartmentDto(DepartmentDto departmentDto){
+        return departmentDto;
+    }
 }
--- src/main/java/com/likelion/springlogging/service/Imp/EmployeeServiceImp.java
+++ src/main/java/com/likelion/springlogging/service/Imp/EmployeeServiceImp.java
@@ -1,2 +1,16 @@
-package com.likelion.springlogging.service.Imp;public class EmployeeServiceImp {
+package com.likelion.springlogging.service.Imp;
+
+import com.likelion.springlogging.dto.EmployeeDto;
+import com.likelion.springlogging.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImp implements EmployeeService {
+    @Autowired
+    EmployeeDto employeeDto;
+    @Override
+    public EmployeeDto getEmployeeDto(EmployeeDto employeeDto){
+        return employeeDto;
+    }
 }
