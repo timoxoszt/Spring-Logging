dd5afc81fc2c4a1860a3361d7937f88aa5ce13e2
timoxoszt <20520810@gm.uit.edu.vn>
2022-09-08 00:37:38 +0700

    service

---
+++ src/main/java/com/likelion/springlogging/service/DepartmentService.java
@@ -0,0 +1,7 @@
+package com.likelion.springlogging.service;
+
+import com.likelion.springlogging.dto.DepartmentDto;
+
+public interface DepartmentService {
+    DepartmentDto getDepartmentDto(DepartmentDto departmentDto);
+}
---
+++ src/main/java/com/likelion/springlogging/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.likelion.springlogging.service;
+
+import com.likelion.springlogging.dto.EmployeeDto;
+
+public interface EmployeeService {
+    EmployeeDto getEmployeeDto(EmployeeDto employeeDto);
+}
